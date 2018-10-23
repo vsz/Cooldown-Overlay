a1939e512a17be5bd8add705c25a85901d50ddf3
Vítor Zeni <vsz@certi.org.br>
2018-10-23 13:27:58 -0300

    Cleaning up a bit and creating WindowHandler class

--- cd-overlay.py
+++ cd-overlay.py
@@ -75,273 +75,6 @@ equipmentSlotList.append(TrackedEquipmentSlot(EquipmentType.LEGS))
 # Separators for the tracked actions section
 emptyLines = [2,4,6,8];
 
-def createWindow():
-	#get instance handle
-	hInstance = win32api.GetModuleHandle()
-
-	# the class name
-	className = 'Cooldown Overlay'
-
-	# create and initialize window class
-	wndClass				= win32gui.WNDCLASS()
-	wndClass.style			= win32con.CS_HREDRAW | win32con.CS_VREDRAW
-	wndClass.lpfnWndProc	= wndProc
-	wndClass.hInstance		= hInstance
-	wndClass.hIcon			= win32gui.LoadIcon(0, win32con.IDI_APPLICATION)
-	wndClass.hCursor		= win32gui.LoadCursor(None, win32con.IDC_ARROW)
-	wndClass.hbrBackground	= win32gui.GetStockObject(win32con.WHITE_BRUSH)
-	wndClass.lpszClassName	= className
-
-	# register window class
-	wndClassAtom = None
-	try:
-		wndClassAtom = win32gui.RegisterClass(wndClass)
-	except Exception as e:
-		print (e)
-		raise e
-
-	# http://msdn.microsoft.com/en-us/library/windows/desktop/ff700543(v=vs.85).aspx
-	# Consider using: WS_EX_COMPOSITED, WS_EX_LAYERED, WS_EX_NOACTIVATE, WS_EX_TOOLWINDOW, WS_EX_TOPMOST, WS_EX_TRANSPARENT
-	# The WS_EX_TRANSPARENT flag makes events (like mouse clicks) fall through the window.
-	exStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
-
-	# http://msdn.microsoft.com/en-us/library/windows/desktop/ms632600(v=vs.85).aspx
-	# Consider using: WS_DISABLED, WS_POPUP, WS_VISIBLE
-	style = win32con.WS_DISABLED | win32con.WS_POPUP | win32con.WS_VISIBLE
-
-	hWindow = win32gui.CreateWindowEx(
-		exStyle,
-		wndClassAtom,				   #it seems message dispatching only works with the atom, not the class name
-		None, #WindowName
-		style,
-		0, # x
-		0, # y
-		win32api.GetSystemMetrics(win32con.SM_CXSCREEN), # width
-		win32api.GetSystemMetrics(win32con.SM_CYSCREEN), # height
-		None, # hWndParent
-		None, # hMenu
-		hInstance,
-		None # lpParam
-	)
-
-	# http://msdn.microsoft.com/en-us/library/windows/desktop/ms633540(v=vs.85).aspx
-	win32gui.SetLayeredWindowAttributes(hWindow, 0x00ffffff, 255, win32con.LWA_COLORKEY | win32con.LWA_ALPHA)
-
-	# http://msdn.microsoft.com/en-us/library/windows/desktop/dd145167(v=vs.85).aspx
-	#win32gui.UpdateWindow(hWindow)
-
-	# http://msdn.microsoft.com/en-us/library/windows/desktop/ms633545(v=vs.85).aspx
-	win32gui.SetWindowPos(hWindow, win32con.HWND_TOPMOST, 0, 0, 0, 0,
-		win32con.SWP_NOACTIVATE | win32con.SWP_NOMOVE | win32con.SWP_NOSIZE | win32con.SWP_SHOWWINDOW)
-
-	# http://msdn.microsoft.com/en-us/library/windows/desktop/ms633548(v=vs.85).aspx
-	#win32gui.ShowWindow(hWindow, win32con.SW_SHOW)
-
-
-	# Show & update the window
-	win32gui.ShowWindow(hWindow, win32con.SW_SHOWNORMAL)
-	win32gui.UpdateWindow(hWindow)
-
-	return hWindow
-
-def drawTextLabel(hdc, pos, color=ColorCode.BLACK,text=''):
-	win32gui.SetTextColor(hdc, color)
-	win32gui.DrawText(hdc,text,-1,pos,win32con.DT_LEFT | win32con.DT_VCENTER)
-
-def drawTimerLabel(hdc, pos, color=ColorCode.BLACK,initialValue=0.0):
-	win32gui.SetTextColor(hdc, color)
-	win32gui.DrawText(hdc,'{0:.1f}'.format(initialValue),-1,pos,win32con.DT_RIGHT | win32con.DT_VCENTER)
-
-def drawRightArc(hdc, pos, radius, width, span, percent, color=ColorCode.BLACK):
-	
-	if percent > 0.0:
-		# Get arc center coordinates
-		xc = pos[0]
-		yc = pos[1]
-		
-		# Calculate a2
-		a1 = math.radians(span/2)
-		a2 = math.asin(radius*math.sin(a1)/(radius+width))
-		
-		# calculate displacement on x and y axis
-		dx = radius*math.cos(a2)
-		dy = radius*math.sin(a2)
-		
-		# calculate starting point coordinates
-		xs = int(xc + dx)
-		ys = int(yc + dy)
-		a2d = math.degrees(a2)
-		
-		# determines arc span
-		span = 2*a2d
-		
-		# sets the percentage to be drawn
-		n = percent
-		
-		# sets arc fill color
-		sb = win32gui.CreateSolidBrush(color)
-		win32gui.SelectObject(hdc, sb)
-		
-		# sets arc stroke
-		sp = win32gui.CreatePen(win32con.PS_SOLID,1,ColorCode.BLACK)
-		win32gui.SelectObject(hdc,sp)
-
-		# draws the outline of the arc
-		win32gui.BeginPath(hdc)
-		win32gui.MoveToEx(hdc,xs,ys)
-		win32gui.AngleArc(hdc,xc,yc,radius+width,-a2d,int(n*span))
-		win32gui.AngleArc(hdc,xc,yc,radius,int(a2d-(1-n)*span),-int(n*span))
-		win32gui.EndPath(hdc)
-		
-		# fills the arc with outer stroke
-		win32gui.StrokeAndFillPath(hdc)
-	
-		# returns the next arc span and radius
-		return span,(radius+width)
-	else:
-		# if its not drawn, returns original arc span and radius
-		return span,radius
-
-
-def drawLeftArc(hdc, pos, radius, width, span, percent, color=ColorCode.BLACK):
-	
-	if percent > 0.0:
-		# Get arc center coordinates
-		xc = pos[0]
-		yc = pos[1]
-		
-		# Calculate a2
-		a1 = math.radians(span/2)
-		a2 = math.asin(radius*math.sin(a1)/(radius+width))
-		
-		# calculate displacement on x and y axis
-		dx = radius*math.cos(a2)
-		dy = radius*math.sin(a2)
-		
-		# calculate starting point coordinates
-		xs = int(xc - dx)
-		ys = int(yc + dy)
-		a2d = math.degrees(a2)
-		
-		# determines arc span
-		span = 2*a2d
-		
-		# sets the percentage to be drawn
-		n = percent
-		
-		# sets arc fill color
-		sb = win32gui.CreateSolidBrush(color)
-		win32gui.SelectObject(hdc, sb)
-
-		# draws the outline of the arc
-		win32gui.BeginPath(hdc)
-		win32gui.MoveToEx(hdc,xs,ys)
-		win32gui.AngleArc(hdc,xc,yc,radius+width,180+a2d,-int(n*span))
-		win32gui.AngleArc(hdc,xc,yc,radius,int(180-a2d+(1-n)*span),int(n*span))
-		win32gui.EndPath(hdc)
-		
-		# fills the arc with outer stroke
-		win32gui.StrokeAndFillPath(hdc)
-	
-		# returns the next arc span and radius
-		return span,(radius+width)
-	else:
-		# if its not drawn, returns original arc span and radius
-		return span,radius
-
-def wndProc(hWnd, message, wParam, lParam):
-
-	if message == win32con.WM_PAINT:
-		hdc, paintStruct = win32gui.BeginPaint(hWnd)
-
-		# Font configuration
-		dpiScale = win32ui.GetDeviceCaps(hdc, win32con.LOGPIXELSX) / 60.0
-		fontSize = 10
-
-		# http://msdn.microsoft.com/en-us/library/windows/desktop/dd145037(v=vs.85).aspx
-		lf = win32gui.LOGFONT()
-		lf.lfFaceName = "Tahoma"
-		lf.lfHeight = int(round(dpiScale * fontSize))
-		lf.lfWeight = 700 # bold
-		lf.lfQuality = win32con.NONANTIALIASED_QUALITY # Use nonantialiased to remove the white edges around the text.
-
-		hf = win32gui.CreateFontIndirect(lf)
-
-		win32gui.SelectObject(hdc, hf)
-		
-		br=win32gui.CreateSolidBrush(win32api.RGB(255,0,0))
-		win32gui.SelectObject(hdc, br)
-		
-		# Get relative dimensions
-		rect = win32gui.GetClientRect(hWnd)
-		w = rect[2]
-		h = rect[3]
-
-		# Bars
-		# Positions the bars
-		xc = int(w/2.033)
-		yc = int(h/2.522)
-		r = 171
-		dr = 12
-		alpha = 90
-		
-		## Text
-		# Positions the text
-		pleft = int(0.752*w)
-		ptop = int(0.1*h)
-		pright = int(0.8165*w)
-		pbottom = int(0.55*h)
-		spc = int(0.015*h)
-		
-		# Filter what do draw
-		groupsToDraw = [g for g in groupList if g.visible]
-		actionsToDraw = [a for a in actionList if a.visible]
-		equipmentToDraw = [e for e in equipmentList if e.visible]
-		rightArcToDraw = [a for a in actionList if a.arcPlacement == ArcPlacement.RIGHT]
-		leftArcToDraw = [a for a in actionList if a.arcPlacement == ArcPlacement.LEFT]
-
-		sr = alpha
-		rr = r
-		for idx,action in enumerate(rightArcToDraw):
-			sr,rr = drawRightArc(hdc,(xc,yc),rr,dr,sr,action.getPercentage(), action.color)
-
-		sl = alpha
-		rl = r
-		for idx,action in enumerate(leftArcToDraw):
-			sl,rl = drawLeftArc(hdc,(xc,yc),rl,dr,sl,action.getPercentage(), action.color)
-
-		for idx,group in enumerate(groupsToDraw):
-			pos = (pleft,ptop+idx*spc,pright,pbottom)
-			drawTextLabel(hdc,pos,group.color,group.labelText)
-			drawTimerLabel(hdc,pos,group.color,group.countdown)		
-		
-		k=idx+2
-		for idx,action in enumerate(actionsToDraw):
-			if (idx+1) in emptyLines : k=k+1
-			
-			pos = (pleft,ptop+(idx+k)*spc,pright,pbottom)
-			drawTextLabel(hdc,pos,action.color,action.labelText)
-			drawTimerLabel(hdc,pos,action.color,action.countdown)
-
-		k=k+idx+2
-		for idx,equip in enumerate(equipmentToDraw):
-			#if (idx+1) in emptyLines : k=k+1
-
-			pos = (pleft,ptop+(idx+k)*spc,pright,pbottom)
-			drawTextLabel(hdc,pos,equip.color,equip.labelText)
-			drawTimerLabel(hdc,pos,equip.color,equip.countdown)
-			
-
-		win32gui.EndPaint(hWnd, paintStruct)
-		return 0
-
-	elif message == win32con.WM_DESTROY:
-		win32gui.PostQuitMessage(0)
-		return 0
-
-	else:
-		return win32gui.DefWindowProc(hWnd, message, wParam, lParam)
-
 def debugMode():
 	print('Debug Mode: press hotkeys to see how to add them in the script')
 	while(True):
@@ -354,7 +87,7 @@ def main():
 		debugMode()
 	
 	# Create transparent window
-	hWindow = createWindow()
+	windowHandler = WindowHandler(actionList,groupList,equipmentList,emptyLines)
 
 	# Create threads
 	# Thread that detects keyboard hotkeys
@@ -371,7 +104,7 @@ def main():
 
 	try:
 		while(True):
-			win32gui.RedrawWindow(hWindow, None, None, win32con.RDW_INVALIDATE | win32con.RDW_ERASE)
+			win32gui.RedrawWindow(windowHandler.hWindow, None, None, win32con.RDW_INVALIDATE | win32con.RDW_ERASE)
 			win32gui.PumpWaitingMessages()
 			time.sleep(0.05)
 		
@@ -388,7 +121,7 @@ def main():
 		tHotkeyTracker.join()	
 		print("Hotkey Tracker Stopped")
 
-		win32gui.DestroyWindow(hWindow)
+		win32gui.DestroyWindow(windowHandler.hWindow)
 		print("Overlay window destroyed")
 		print("Closing...")	
 
--- classes.py
+++ classes.py
@@ -8,6 +8,7 @@ import win32api
 import win32con
 import win32gui
 import win32ui
+import math
 
 class ColorCode:		
 	# Colors
@@ -77,6 +78,281 @@ class EquipmentType(Enum):
 class UseType(Enum):
 	TARGET = 1
 	CROSSHAIR = 2
+
+class WindowHandler:
+	def __init__(self,actionList,groupList,equipmentList,emptyLines):
+		self.hWindow = self.createWindow()
+		self.actionList = actionList
+		self.groupList = groupList
+		self.equipmentList = equipmentList
+		self.emptyLines = emptyLines
+
+	def createWindow(self):
+		#get instance handle
+		hInstance = win32api.GetModuleHandle()
+
+		# the class name
+		className = 'Cooldown Overlay'
+
+		# create and initialize window class
+		wndClass				= win32gui.WNDCLASS()
+		wndClass.style			= win32con.CS_HREDRAW | win32con.CS_VREDRAW
+		wndClass.lpfnWndProc	= self.wndProc
+		wndClass.hInstance		= hInstance
+		wndClass.hIcon			= win32gui.LoadIcon(0, win32con.IDI_APPLICATION)
+		wndClass.hCursor		= win32gui.LoadCursor(None, win32con.IDC_ARROW)
+		wndClass.hbrBackground	= win32gui.GetStockObject(win32con.WHITE_BRUSH)
+		wndClass.lpszClassName	= className
+
+		# register window class
+		wndClassAtom = None
+		try:
+			wndClassAtom = win32gui.RegisterClass(wndClass)
+		except Exception as e:
+			print (e)
+			raise e
+
+		# http://msdn.microsoft.com/en-us/library/windows/desktop/ff700543(v=vs.85).aspx
+		# Consider using: WS_EX_COMPOSITED, WS_EX_LAYERED, WS_EX_NOACTIVATE, WS_EX_TOOLWINDOW, WS_EX_TOPMOST, WS_EX_TRANSPARENT
+		# The WS_EX_TRANSPARENT flag makes events (like mouse clicks) fall through the window.
+		exStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
+
+		# http://msdn.microsoft.com/en-us/library/windows/desktop/ms632600(v=vs.85).aspx
+		# Consider using: WS_DISABLED, WS_POPUP, WS_VISIBLE
+		style = win32con.WS_DISABLED | win32con.WS_POPUP | win32con.WS_VISIBLE
+
+		hWindow = win32gui.CreateWindowEx(
+			exStyle,
+			wndClassAtom,				   #it seems message dispatching only works with the atom, not the class name
+			None, #WindowName
+			style,
+			0, # x
+			0, # y
+			win32api.GetSystemMetrics(win32con.SM_CXSCREEN), # width
+			win32api.GetSystemMetrics(win32con.SM_CYSCREEN), # height
+			None, # hWndParent
+			None, # hMenu
+			hInstance,
+			None # lpParam
+		)
+
+		# http://msdn.microsoft.com/en-us/library/windows/desktop/ms633540(v=vs.85).aspx
+		win32gui.SetLayeredWindowAttributes(hWindow, 0x00ffffff, 255, win32con.LWA_COLORKEY | win32con.LWA_ALPHA)
+
+		# http://msdn.microsoft.com/en-us/library/windows/desktop/dd145167(v=vs.85).aspx
+		#win32gui.UpdateWindow(hWindow)
+
+		# http://msdn.microsoft.com/en-us/library/windows/desktop/ms633545(v=vs.85).aspx
+		win32gui.SetWindowPos(hWindow, win32con.HWND_TOPMOST, 0, 0, 0, 0,
+			win32con.SWP_NOACTIVATE | win32con.SWP_NOMOVE | win32con.SWP_NOSIZE | win32con.SWP_SHOWWINDOW)
+
+		# http://msdn.microsoft.com/en-us/library/windows/desktop/ms633548(v=vs.85).aspx
+		#win32gui.ShowWindow(hWindow, win32con.SW_SHOW)
+
+
+		# Show & update the window
+		win32gui.ShowWindow(hWindow, win32con.SW_SHOWNORMAL)
+		win32gui.UpdateWindow(hWindow)
+
+		return hWindow
+
+	def drawTextLabel(self, hdc, pos, color=ColorCode.BLACK,text=''):
+		win32gui.SetTextColor(hdc, color)
+		win32gui.DrawText(hdc,text,-1,pos,win32con.DT_LEFT | win32con.DT_VCENTER)
+
+	def drawTimerLabel(self, hdc, pos, color=ColorCode.BLACK,initialValue=0.0):
+		win32gui.SetTextColor(hdc, color)
+		win32gui.DrawText(hdc,'{0:.1f}'.format(initialValue),-1,pos,win32con.DT_RIGHT | win32con.DT_VCENTER)
+
+	def drawRightArc(self, hdc, pos, radius, width, span, percent, color=ColorCode.BLACK):
+		
+		if percent > 0.0:
+			# Get arc center coordinates
+			xc = pos[0]
+			yc = pos[1]
+			
+			# Calculate a2
+			a1 = math.radians(span/2)
+			a2 = math.asin(radius*math.sin(a1)/(radius+width))
+			
+			# calculate displacement on x and y axis
+			dx = radius*math.cos(a2)
+			dy = radius*math.sin(a2)
+			
+			# calculate starting point coordinates
+			xs = int(xc + dx)
+			ys = int(yc + dy)
+			a2d = math.degrees(a2)
+			
+			# determines arc span
+			span = 2*a2d
+			
+			# sets the percentage to be drawn
+			n = percent
+			
+			# sets arc fill color
+			sb = win32gui.CreateSolidBrush(color)
+			win32gui.SelectObject(hdc, sb)
+			
+			# sets arc stroke
+			sp = win32gui.CreatePen(win32con.PS_SOLID,1,ColorCode.BLACK)
+			win32gui.SelectObject(hdc,sp)
+
+			# draws the outline of the arc
+			win32gui.BeginPath(hdc)
+			win32gui.MoveToEx(hdc,xs,ys)
+			win32gui.AngleArc(hdc,xc,yc,radius+width,-a2d,int(n*span))
+			win32gui.AngleArc(hdc,xc,yc,radius,int(a2d-(1-n)*span),-int(n*span))
+			win32gui.EndPath(hdc)
+			
+			# fills the arc with outer stroke
+			win32gui.StrokeAndFillPath(hdc)
+		
+			# returns the next arc span and radius
+			return span,(radius+width)
+		else:
+			# if its not drawn, returns original arc span and radius
+			return span,radius
+
+	def drawLeftArc(self, hdc, pos, radius, width, span, percent, color=ColorCode.BLACK):
+		
+		if percent > 0.0:
+			# Get arc center coordinates
+			xc = pos[0]
+			yc = pos[1]
+			
+			# Calculate a2
+			a1 = math.radians(span/2)
+			a2 = math.asin(radius*math.sin(a1)/(radius+width))
+			
+			# calculate displacement on x and y axis
+			dx = radius*math.cos(a2)
+			dy = radius*math.sin(a2)
+			
+			# calculate starting point coordinates
+			xs = int(xc - dx)
+			ys = int(yc + dy)
+			a2d = math.degrees(a2)
+			
+			# determines arc span
+			span = 2*a2d
+			
+			# sets the percentage to be drawn
+			n = percent
+			
+			# sets arc fill color
+			sb = win32gui.CreateSolidBrush(color)
+			win32gui.SelectObject(hdc, sb)
+
+			# draws the outline of the arc
+			win32gui.BeginPath(hdc)
+			win32gui.MoveToEx(hdc,xs,ys)
+			win32gui.AngleArc(hdc,xc,yc,radius+width,180+a2d,-int(n*span))
+			win32gui.AngleArc(hdc,xc,yc,radius,int(180-a2d+(1-n)*span),int(n*span))
+			win32gui.EndPath(hdc)
+			
+			# fills the arc with outer stroke
+			win32gui.StrokeAndFillPath(hdc)
+		
+			# returns the next arc span and radius
+			return span,(radius+width)
+		else:
+			# if its not drawn, returns original arc span and radius
+			return span,radius
+
+	def wndProc(self,hWnd,message, wParam, lParam):
+
+		if message == win32con.WM_PAINT:
+			hdc, paintStruct = win32gui.BeginPaint(hWnd)
+
+			# Font configuration
+			dpiScale = win32ui.GetDeviceCaps(hdc, win32con.LOGPIXELSX) / 60.0
+			fontSize = 10
+
+			# http://msdn.microsoft.com/en-us/library/windows/desktop/dd145037(v=vs.85).aspx
+			lf = win32gui.LOGFONT()
+			lf.lfFaceName = "Tahoma"
+			lf.lfHeight = int(round(dpiScale * fontSize))
+			lf.lfWeight = 700 # bold
+			lf.lfQuality = win32con.NONANTIALIASED_QUALITY # Use nonantialiased to remove the white edges around the text.
+
+			hf = win32gui.CreateFontIndirect(lf)
+
+			win32gui.SelectObject(hdc, hf)
+			
+			br=win32gui.CreateSolidBrush(win32api.RGB(255,0,0))
+			win32gui.SelectObject(hdc, br)
+			
+			# Get relative dimensions
+			rect = win32gui.GetClientRect(hWnd)
+			w = rect[2]
+			h = rect[3]
+
+			# Bars
+			# Positions the bars
+			xc = int(w/2.033)
+			yc = int(h/2.522)
+			r = 171
+			dr = 12
+			alpha = 90
+			
+			## Text
+			# Positions the text
+			pleft = int(0.752*w)
+			ptop = int(0.1*h)
+			pright = int(0.8165*w)
+			pbottom = int(0.55*h)
+			spc = int(0.015*h)
+			
+			# Filter what do draw
+			groupsToDraw = [g for g in self.groupList if g.visible]
+			actionsToDraw = [a for a in self.actionList if a.visible]
+			equipmentToDraw = [e for e in self.equipmentList if e.visible]
+			rightArcToDraw = [a for a in self.actionList if a.arcPlacement == ArcPlacement.RIGHT]
+			leftArcToDraw = [a for a in self.actionList if a.arcPlacement == ArcPlacement.LEFT]
+
+			sr = alpha
+			rr = r
+			for idx,action in enumerate(rightArcToDraw):
+				sr,rr = self.drawRightArc(hdc,(xc,yc),rr,dr,sr,action.getPercentage(), action.color)
+
+			sl = alpha
+			rl = r
+			for idx,action in enumerate(leftArcToDraw):
+				sl,rl = self.drawLeftArc(hdc,(xc,yc),rl,dr,sl,action.getPercentage(), action.color)
+
+			for idx,group in enumerate(groupsToDraw):
+				pos = (pleft,ptop+idx*spc,pright,pbottom)
+				self.drawTextLabel(hdc,pos,group.color,group.labelText)
+				self.drawTimerLabel(hdc,pos,group.color,group.countdown)		
+			
+			k=idx+2
+			for idx,action in enumerate(actionsToDraw):
+				if (idx+1) in self.emptyLines : k=k+1
+				
+				pos = (pleft,ptop+(idx+k)*spc,pright,pbottom)
+				self.drawTextLabel(hdc,pos,action.color,action.labelText)
+				self.drawTimerLabel(hdc,pos,action.color,action.countdown)
+
+			k=k+idx+2
+			for idx,equip in enumerate(equipmentToDraw):
+				#if (idx+1) in emptyLines : k=k+1
+
+				pos = (pleft,ptop+(idx+k)*spc,pright,pbottom)
+				self.drawTextLabel(hdc,pos,equip.color,equip.labelText)
+				self.drawTimerLabel(hdc,pos,equip.color,equip.countdown)
+				
+
+			win32gui.EndPaint(hWnd, paintStruct)
+			return 0
+
+		elif message == win32con.WM_DESTROY:
+			win32gui.PostQuitMessage(0)
+			return 0
+
+		else:
+			return win32gui.DefWindowProc(hWnd, message, wParam, lParam)
+	
 
 class ActionTracker(threading.Thread):
 	def __init__(self,actionList,equipmentList,groupList,equipmentSlotList):
